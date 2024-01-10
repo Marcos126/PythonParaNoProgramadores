545fc960b373816b42c8064e7970cf7cc6e11891
Marcos126 <natalemarcos06@gmail.com>
2024-01-10 00:02:25 -0300

    first commit

---
+++ clase5/listas.py
@@ -0,0 +1,13 @@
+"""
+Listas:
+
+1- El orden es importante
+2- Los elementos de la lista son mutables
+3- Se pueden repetir elementos
+"""
+
+li = [1, 2, 3, 4, 5, 6, 10, -1, -4, -6]
+
+print(type(li))
+
+print("done")
--- clase5/tablas.py
+++ clase5/tablas.py
@@ -1,18 +1,13 @@
 n = input("Ingrese la tabla a consultar")
 i = 0
 
+n = int(n)
+
 while True:
-    print(n, "No es un numero")
-    n = input("Por favor ingrese un numero")
-    n = type(n)
-    if type(n) == int:
+    x = n * i
+    print(n, "x", i, "=", x)
+    i = i + 1
+    if i == 21:
         break
-print("done")
-# while True:
-# x = n * i
-# i = i + 1
-# print(n, "+", i, "=", x)
-# if i == 20:
-# break
 
 print("done")
