ee0d67fdb73974a63bd1ffc594f79422bcf47e13
Marcos126 <natalemarcos06@gmail.com>
2024-01-09 22:52:39 -0300

    tarea uwu

---
+++ clase5/tablas.py
@@ -0,0 +1,18 @@
+n = input("Ingrese la tabla a consultar")
+i = 0
+
+while True:
+    print(n, "No es un numero")
+    n = input("Por favor ingrese un numero")
+    n = type(n)
+    if type(n) == int:
+        break
+print("done")
+# while True:
+# x = n * i
+# i = i + 1
+# print(n, "+", i, "=", x)
+# if i == 20:
+# break
+
+print("done")
