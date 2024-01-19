983c703e9d737cf051e444703d018a7cf3f9d989
Marcos126 <natalemarcos06@gmail.com>
2024-01-19 15:38:15 -0300

    tarea uwu

--- Laboratorios/Modulo2/Integrador.py
+++ Laboratorios/Modulo2/Integrador.py
@@ -1,26 +1,45 @@
-lista = []
-cursos = []
+# Declaracion de variables de la lista de los alumnos y la cantidad de cursos
+
+listaNombres = []
+cantidadCursos = []
+# Lista con bucle while para que te devuelva al menu cada vez que termines de ingresar la operacion
 
 while True:
-    print("Ingrese el numero de la operacion que desea ejecutar:")
+    contador = 0
+    print("\n Ingrese el numero de la operacion que desea ejecutar:")
     print("1 - Ver la lista de alumno")
     print("2 - Añadir un alumno a la lista.")
     print("3 - Salir.")
+
+    # seleccionador
 
     seleccion = input()
 
     seleccion = int(seleccion)
 
+    # ifs para hacer uso del seleccionador
+
     if seleccion == 1:
-        print(lista)
+        if len(listaNombres) == 0:
+            print("Todavia no hay nadie inscripto en el sistema \n")
+        else:
+            while contador != len(cantidadCursos):
+                print(
+                    "El alumno ",
+                    listaNombres[contador],
+                    "esta inscripto a ",
+                    cantidadCursos[contador],
+                    "cursos!",
+                )
+                contador = contador + 1
     elif seleccion == 2:
         print("ingrese el nombre que desea agregar a la lista")
         nombreAlumno = input()
         print("Ingrese a cuantos cursos esta inscripto")
         cursosCantidad = input()
-        lista.append(nombreAlumno)
-        cursos.append(cursosCantidad)
-        print("Se ha agregado", nombreAlumno, "Con", cursosCantidad, "!")
+        listaNombres.append(nombreAlumno)
+        cantidadCursos.append(cursosCantidad)
+        print("Se ha agregado", nombreAlumno, "Con", cursosCantidad, "cursos! \n")
     elif seleccion == 3:
         print("Adios")
         break
---
+++ RepasosPersonales/ListasDobles.py
@@ -0,0 +1,18 @@
+listaNombres = []
+listaApellidos = []
+contador = 0
+
+listaNombres.append("Martin")
+listaNombres.append("Pedro")
+listaNombres.append("Rodrigo")
+
+
+listaApellidos.append("Martinez")
+listaApellidos.append("Rodriguez")
+listaApellidos.append("Natale")
+
+while contador != len(listaNombres):
+    print(listaNombres[contador], listaApellidos[contador])
+    contador = contador + 1
+
+print("done!")
